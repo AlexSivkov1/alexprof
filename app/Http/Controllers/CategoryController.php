98df5d3050fe4ea795bc49f966041ed7b5f55bdd
alex <plingsir2012@gmail.com>
2021-06-28 22:20:44 +0500

    доделал ВЕРСТКУ-выводПостовПоЗапрошеннойКатегории

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -23,12 +23,15 @@ class CategoryController extends Controller
 
     public function show($slug)
     {
+        /* $categories = Category::pluck('title', 'slug')->all();
+    $posts = Post::with('category')->orderBy('id', 'desc')->paginate(15);
+    $post = Category::where('slug', $slug)->firstOrFail();*/
 
-        $category = Category::all();
-        $categories = Category::pluck('title', 'slug')->all();
-        $posts = Post::with('category')->orderBy('id', 'desc')->paginate(15);
-        $post = Category::where('slug', $slug)->firstOrFail();
-        return view('cats.show', compact('categories', 'posts', 'post', 'category'));
+        $category = Category::where('slug', $slug)->firstOrFail();
+        $posts = $category->posts()->paginate(3);
+
+
+        return view('cats.show', compact( 'posts', 'category'));
 
     }
 }
--- resources/views/cats/show.blade.php
+++ resources/views/cats/show.blade.php
@@ -1,47 +1,84 @@
 @extends('layouts.layout')
 
-@section('title', 'AlexProf: ' . $post->title)
+@section('title', 'AlexProf: ' . $category->title)
 
 
 @section('content')
-    <div class="section animated-row" >
-        <div class="section-inner">
-            <div class="row justify-content-center">
-                <div class="col-md-8 wide-col-laptop">
-                    <div class="title-block animate" data-animate="fadeInUp">
-                        {{--<h6 class="col-md-7" style="margin-top:12px">{{ $category->title }}</h6>--}}
-                    </div>
-                    <div class="col-md-8 offset-md-2">
-                        <div class="testimonials-section">
-                            <div class="testimonial-item">
-                                <div class="client-row">
-                                    {{--<img src="{{ $post->getImage() }}">--}}
-                                </div>
-                                <div class="testimonial-content">
-                                   {{-- <h6><a href="{{ route('category.single', ['slug'=>$post->category->slug]) }}" ></a></h6>--}}
-                               {{--     <small class="font-weight-bold">{{ $post->title }}</small>
-                                    <small>{{ $post->category->title }}</small>
-                                    <small>{{ $post->getPostDate() }}</small>
-                                    <small>{{ $post->description }}</small>
-                                    <small><i class="fa fa-eye"></i>{{ $post->views }}</small>--}}
 
+    {{--  <div class="page-wrapper">
+          <div class="blog-custom-build">
 
+              @foreach($posts as $post)
+                  <div class="blog-box wow fadeIn">
+                      <div class="post-media">
+                          <a href="{{ route('posts.single', ['slug' => $post->slug]) }}" title="">
+                              <img src="{{ $post->getImage() }}" alt="" class="img-fluid">
+                              <div class="hovereffect">
+                                  <span></span>
+                              </div>
+                              <!-- end hover -->
+                          </a>
+                      </div>
+                      <!-- end media -->
+                      <div class="blog-meta big-meta text-center">
+                          <div class="post-sharing">
+                              <ul class="list-inline">
+                                  <li><a href="#" class="fb-button btn btn-primary"><i class="fa fa-facebook"></i> <span
+                                                  class="down-mobile">Share on Facebook</span></a></li>
+                                  <li><a href="#" class="tw-button btn btn-primary"><i class="fa fa-twitter"></i> <span
+                                                  class="down-mobile">Tweet on Twitter</span></a></li>
+                                  <li><a href="#" class="gp-button btn btn-primary"><i class="fa fa-google-plus"></i></a>
+                                  </li>
+                              </ul>
+                          </div><!-- end post-sharing -->
+
+                          <h4><a href="{{ route('posts.single', ['slug' => $post->slug]) }}" title="">{{ $post->title }}</a></h4>
+                          {!! $post->description !!}
+                          <small><a href="{{ route('categories.single', ['slug' => $post->category->slug]) }}" title="">{{ $post->category->title }}</a></small>
+                          <small>{{ $post->getPostDate() }}</small>
+                          <small><i class="fa fa-eye"></i> {{ $post->views }}</small>
+                      </div><!-- end meta -->
+                  </div><!-- end blog-box -->
+
+                  <hr class="invis">
+              @endforeach
+
+          </div>
+      </div>--}}
+    <div class="row justify-content-center">
+        <div class="title-block" data-animate="fadeInUp"  style="margin-top:2%;">
+            <span>Посты в категории <br>{{ $category->title }} :</span>
+        </div>
+        <div class="col-md-8 wide-col-laptop" style="margin-left: 10%">
+            <div class="gallery-section">
+                <div class="gallery-list owl-carousel owl-loaded owl-drag">
+
+                    <div class="owl-stage-outer" >
+                        <div class="owl-stage "
+                             style="transform: translate3d(-2242px, 0px, 0px); transition: all 0.7s ease 0s; width: 3364px;">
+                            <div class="owl-item active" style="width: 343.667px;">
+                                @foreach($posts as $post)
+                                <div class="item">
+                                    <div class="portfolio-item" style="width: 340px" >
+                                        <div class="thumb" style="margin-left: 20px">
+                                            <img src="{{ $post->getImage() }}" alt="">
+                                        </div>
+                                        <div class="thumb-inner" data-animate="fadeInUp" >
+                                            <h4>{{ $post->title }}t</h4>
+                                            <p>{{ $post->description }}</p>
+                                        </div>
+                                    </div>
                                 </div>
+                                    @endforeach
                             </div>
                         </div>
                     </div>
+
+
                 </div>
             </div>
         </div>
- {{--       @if($post->tags->count())
-            <span class="font-weight-bold">Теги:  </span>
-            @foreach($post->tags as $tag)
-                <small><a href="{{ route('tags.single', ['slug'=>$tag->slug]) }}" title="">{{ $tag->title }}</a></small>
-            @endforeach
-
-        @endif--}}
     </div>
+
+
 @endsection
-
-
-
